a46fac69f72d31dc9facaad55124b4aba0d8ee61
kirby-b <kirby.brodie66@gmail.com>
2023-09-02 20:15:40 -0400

    completed for the most part
    makes a file that is ready to be molded into a program.
    -Includes example

---
+++ file.py
@@ -0,0 +1,6 @@
+def main():
+    pass
+def newFunc():
+    pass
+if __name__ == "__main__":
+    main()
--- main/main.py
+++ main/main.py
@@ -1,7 +1,5 @@
 def main():
-  print(
-      "This program will create a .py file with basic setup. Dont give it a file extension"
-  )
+  print("This program will create a .py file with basic setup. Dont give it a file extension")
   name = input("File Name: ")
   name = name + ".py"
   makeFile(name)
@@ -9,8 +7,13 @@ def main():
 
 def makeFile(name):
   f = open(name, "w")
-  f.write("Setup will go here")
+  f.write("""def main():
+    pass
+def newFunc():
+    pass
+if __name__ == "__main__":
+    main()""")
 
 
-if __name__ == "main":
-  main()
+if __name__ == "__main__":
+    main()
